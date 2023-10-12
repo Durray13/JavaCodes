1c0e3e1c41580641a13eec678990fbf0cd99b994
DurrayNayab22 <durraynayab2287@gmail.com>
2023-10-12 21:51:24 +0530

    Adding a Java code to find the last digit of a number.

---
+++ src/math_operations/LastDigit.java
@@ -0,0 +1,11 @@
+package math_operations;
+
+public class LastDigit {
+
+	public static void main(String[] args) {
+		int num=986678;
+		int l=num%10;
+		System.out.println("Last Digit: "+l);
+	}
+
+}
