c9f9917b31949cf15dcbfeedc13e12d7477543be
DurrayNayab22 <durraynayab2287@gmail.com>
2023-10-12 21:46:19 +0530

    We want to check if divisibility by three comes with its source and math operations project. and package.

---
+++ src/math_operations/DivisibiltyBy3.java
@@ -0,0 +1,12 @@
+package math_operations;
+
+public class DivisibiltyBy3 {
+
+	public static void main(String[] args) {
+		int num=98584;
+		String result=num%3==0?"Divisible by 3":"Not divisible by 3";
+		System.out.println(result);
+
+	}
+
+}
